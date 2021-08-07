1fa0c17882de6458cb42cb7fb08b5bbe83ea2698
Leo Miranda <leomir2656gmail.com>
2021-08-06 21:37:43 -0400

    logeo de usuario

--- ecommerce/src/api/user.js
+++ ecommerce/src/api/user.js
@@ -17,3 +17,21 @@ export async function registerApi(formData){
         console.log(error)
     }
 }
+
+export async function loginApi(formData){
+    try {
+        const url = `${API_URL}/auth/local`
+        const params = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(formData),
+        };
+        const response = await fetch(url, params);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log(error)
+    }
+}
--- ecommerce/src/components/Auth/LoginForm.js
+++ ecommerce/src/components/Auth/LoginForm.js
@@ -1,15 +1,52 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { View, Text } from 'react-native'
+import {useFormik} from "formik"
+import * as Yup from 'yup'
+import {loginApi}  from "../../api/user"
 import {TextInput, Button} from "react-native-paper"
 import { formStyle } from '../../styles'
 
 export default function LoginForm(props) {
+    const {changeForm} = props
+    const [loading, setLoading] = useState(false)
+
+    const formik = useFormik({
+        initialValues : initialValues(),
+        validationSchema: Yup.object(validateSchema()),
+        onSubmit: async (formData )=>{
+            setLoading(true)
+           try {
+               const response = await loginApi(formData);
+               if(response.statusCode) throw "Error en el usuario o contrasena"
+               console.log(response)
+           } catch (error) {               
+                Toast.show("Error al iniciar sesion",{
+                    position: Toast.positions.CENTER,
+                })
+                setLoading(false)
+           }
+        }
+    })
+
     return (
         <View>
-            <TextInput label="Email o Username" style={formStyle.input}/>
-            <TextInput label="contrasena" style={formStyle.input}/>
-            <Button mode="contained" style={formStyle.btnSucces}>Entrar</Button>
+            <TextInput label="Email o Username" onChangeText={(text)=>formik.setFieldValue("identifier", text)} value={formik.values.identifier} error={formik.errors.identifier}style={formStyle.input}/>
+            <TextInput label="contrasena" onChangeText={(text)=>formik.setFieldValue("password", text)} value={formik.values.password} error={formik.errors.password}style={formStyle.input} secureTextEntry/>
+            <Button mode="contained" style={formStyle.btnSucces} onPress={formik.handleSubmit} loading={loading}>Entrar</Button>
             <Button mode="text"  style={formStyle.btnTextLabel} onPress={changeForm}>Registrarse</Button>
         </View>
     )
+}
+function initialValues(){
+    return {
+        identifier: "",
+        password: "",
+
+    }
+}
+function validateSchema() {
+    return {
+        identifier: Yup.string().required(true),
+        password: Yup.string().required(true),
+    }
 }
